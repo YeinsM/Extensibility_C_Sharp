8ce94216f11414171865948698a1869f25044ebb
YeinsM <wildermancera1@gmail.com>
2022-04-18 11:04:09 -0400

    Finish Solution

---
+++ PeopleViewer/Controllers/PeopleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonReader.Factory;
+using PersonReader.Interface;
+using System.Collections.Generic;
+
+namespace PeopleViewer.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private ReaderFactory factory = new ReaderFactory();
+        public IActionResult UseService()
+        {
+            // File reader
+
+            ViewData["Title"] = "Using a Web Service";
+            return PopulatePeopleView("Service");
+            //Inline temporary variable with Ctrl + .
+        }
+
+        public IActionResult UseCSV()
+        {
+            // File reader
+            ViewData["Title"] = "Using a CSV File";
+            return PopulatePeopleView("CSV");
+        }
+
+        public IActionResult UseSQL()
+        {
+            ViewData["Title"] = "Using SQL Database";
+            return PopulatePeopleView("SQL");
+        }
+
+        private IActionResult PopulatePeopleView(string readerType)
+        {
+            // File reader
+            IPersonReader reader = factory.GetReader(readerType);
+            IEnumerable<Person> people = reader.GetPeople();
+            ViewData["ReaderType"] = reader.GetType().ToString();
+            return View("Index", people);
+        }
+    }
+}
---
+++ PersonReader.Service/ServiceReader.cs
@@ -0,0 +1,39 @@
+﻿using PersonReader.Interface;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+
+namespace PersonReader.Service
+{
+    
+    public class ServiceReader : IPersonReader
+    {
+        /// <summary>
+        /// Method <c>client</c> for use http client without asyncronism.
+        /// </summary>
+        WebClient client = new WebClient();
+        string baseUri = "http://localhost:9874";
+        JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        public IEnumerable<Person> GetPeople()
+        {
+            // Specific name for base url
+            string address = $"{baseUri}/people";
+            // Container for our URL
+            string reply = client.DownloadString(address);
+            // Deserealize the string to C# Object
+            return JsonSerializer.Deserialize<IEnumerable<Person>>(reply, options);
+        }
+
+        public Person GetPerson(int id)
+        {
+            // Specific name for base url
+            string address = $"{baseUri}/people/{id}";
+            // Container for our URL
+            string reply = client.DownloadString(address);
+            // Deserealize the string to C# Object
+            return JsonSerializer.Deserialize<Person>(reply, options);
+        }
+    }
+}
